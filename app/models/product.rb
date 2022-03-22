673720f706f10157dec83d69b47887fbd00dffef
Ebina-S <8s6s7e@gmail.com>
2022-03-22 06:20:50 +0000

    Filter_function

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,19 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many :reviews
   acts_as_likeable
-   def reviews_new
+  
+  PER = 15
+ 
+  scope :display_list, -> (category, page) { 
+    if category != "none"
+      where(category_id: category).page(page).per(PER)
+    else
+      page(page).per(PER)
+    end
+  }
+  
+  def reviews_new
     reviews.new
-   end
+  end
    
 end
