47bea7b629f8641f352fbae8ce0efbd3189d1264
Ebina-S <8s6s7e@gmail.com>
2022-03-31 21:12:55 +0000

    layout_er

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ApplicationRecord
-    has_many :products
-    scope :major_categories, -> { pluck(:major_category_name).uniq }
-    scope :request_category, -> (category) { find(category.to_i) }
+  has_many :products, dependent: :destroy
+
+  extend DisplayList
+  scope :major_categories, -> { pluck(:major_category_name).uniq }
+  scope :request_category, -> (category) { find(category.to_i) }
 end
