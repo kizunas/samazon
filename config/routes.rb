1afc543d04b1374c05cfa868f7db45d4015a04a5
Ebina-S <8s6s7e@gmail.com>
2022-04-04 07:32:29 +0000

    Withdrawal

--- config/routes.rb
+++ config/routes.rb
@@ -46,6 +46,7 @@ Rails.application.routes.draw do
       get "mypage/password", :to => "users#update_password"
       put "mypage/password", :to => "users#update_password"
       get "mypage/favorite", :to => "users#favorite"
+      delete "mypage/delete", :to => "users#destroy"
     end
   end
   
